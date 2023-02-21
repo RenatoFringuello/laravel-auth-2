12c281bc8d70aecc2ee88e03eaa386c08be523df
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-21 17:06:27 +0100

    add getValidatedData method

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -9,6 +9,37 @@ use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
+
+    /**
+     * return the data validated
+     *
+     * @param Request $request
+     * @return Array
+     */
+    protected function getValidatedData(Request $request){
+        $validation = [
+            'title' => "required|max:50",
+            'author_name' => 'required|max:100',
+            'author_lastname' => 'required|max:100',
+            'content' => 'required',
+            'start_date' => 'required|date',
+            'end_date' => 'date|nullable',
+        ];
+        $validationMessages = [
+            'title.required' => 'Il titolo è un campo obbligatorio',
+            'title.max' => 'Hai inserito troppi caratteri in title',
+            'author_name.required' => 'Il nome dell\'autore è un campo obbligatorio',
+            'author_name.max' => 'Hai inserito troppi caratteri per il nome',
+            'author_lastname.required' => 'Il cognome dell\'autore è un campo obbligatorio',
+            'author_lastname.max' => 'Hai inserito troppi caratteri il cognome',
+            'content.required' => 'Il contenuto del progetto è un campo obbligatorio',
+            'start_date.required' => 'La data di inizio è un campo obbligatorio',
+            'start_date.date' => 'La data di inizio che hai scritto non esiste in nessun calendario neanche in quello dei maya',
+            'end_date.date' => 'La data di fine che hai scritto non esiste. Lascia il campo vuoto se il lavoro non è finito',
+        ];
+        return $request->validate($validation, $validationMessages);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -73,28 +104,7 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project  $project)
     {
-        $data = $request->validate(
-            [
-                'title' => "required|max:50",
-                'author_name' => 'required|max:100',
-                'author_lastname' => 'required|max:100',
-                'content' => 'required',
-                'start_date' => 'required|date',
-                'end_date' => 'date|nullable',
-            ],
-            [
-                'title.required' => 'Il titolo è un campo obbligatorio',
-                'title.max' => 'Hai inserito troppi caratteri in title',
-                'author_name.required' => 'Il nome dell\'autore è un campo obbligatorio',
-                'author_name.max' => 'Hai inserito troppi caratteri per il nome',
-                'author_lastname.required' => 'Il cognome dell\'autore è un campo obbligatorio',
-                'author_lastname.max' => 'Hai inserito troppi caratteri il cognome',
-                'content.required' => 'Il contenuto del progetto è un campo obbligatorio',
-                'start_date.required' => 'La data di inizio è un campo obbligatorio',
-                'start_date.date' => 'La data di inizio che hai scritto non esiste in nessun calendario neanche in quello dei maya',
-                'end_date.date' => 'La data di fine che hai scritto non esiste. Lascia il campo vuoto se il lavoro non è finito',
-            ]
-        );
+        $data = $this->getValidatedData($request);
         $data['slug'] = Str::slug($data['title']);
         // dd($data);
         $project->update($data);
